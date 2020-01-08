44782df20c95a0da43a1bb235cd25b0d0696c154
Awren Nuit <54560441+AwrenNuit@users.noreply.github.com>
2020-01-07 22:57:15 -0600

    added math rounding, cleaned up code

--- server/server.js
+++ server/server.js
@@ -12,62 +12,56 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(port, ()=>{
     console.log('server up on', port);
-})
+});
 
 app.post(`/math`, (req, res)=>{
-    console.log('in /math POST');
     num.push(req.body.num1);
     num.push(req.body.num2);
     doMath(req.body.op);
     res.sendStatus(200);
-})
+});
 
 app.post(`/reset`, (req, res)=>{
-    console.log('in /reset POST');
     num = [];
     result = [];
     res.sendStatus(200);
-})
+});
 
 app.get(`/result`, (req, res)=>{
-    console.log('in /result GET');
     res.send(result);
-})
+});
 
 app.get(`/history`, (req, res)=>{
-    console.log('in /history GET');
     res.send(history);
-})
+});
 
 app.get(`/history/:index`, (req, res)=>{
-    console.log('in from /history GET');
     res.send(history[req.params.index]);
-})
+});
 
 app.delete('/history', (req, res)=>{
-    console.log('in /messages DELETE');
     history = [];
-    res.send('deleted');
-})
+    res.sendStatus(200);
+});
 
 function doMath(op){
     result = [];
     switch(op){
         case '+':
             result.push(+num[0] + +num[1]);
-            history.push(`${+num[0]} + ${+num[1]} = ${result}`);
+            history.push(`${+num[0]} + ${+num[1]} = ${Math.round((+result + 0.00001) * 100) / 100}`);
             break;
         case '-':
             result.push(+num[0] - +num[1]);
-            history.push(`${+num[0]} - ${+num[1]} = ${result}`);
+            history.push(`${+num[0]} - ${+num[1]} = ${Math.round((+result + 0.00001) * 100) / 100}`);
             break;
         case '*':
             result.push(+num[0] * +num[1]);
-            history.push(`${+num[0]} * ${+num[1]} = ${result}`);
+            history.push(`${+num[0]} * ${+num[1]} = ${Math.round((+result + 0.00001) * 100) / 100}`);
             break;
         case '/':
             result.push(+num[0] / +num[1]);
-            history.push(`${+num[0]} / ${+num[1]} = ${result}`);
+            history.push(`${+num[0]} / ${+num[1]} = ${Math.round((+result + 0.00001) * 100) / 100}`);
             break;
     }
     num = [];
